8cd987a3d7b80093867a3ad21bcacbf1b357c842
Anirban Das <147873362+anirban222777das@users.noreply.github.com>
2025-08-10 12:03:00 +0530

    Create conversionOperation.js

---
+++ basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let score = true
+let age = 20
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "20" => 20
+// "123abc"=NaN
+// true => 1; false =>0
+let isLoggedIn ="0"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "Anirban" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
