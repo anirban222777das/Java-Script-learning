d7b234f2463ea4804c0408ebef7c3c182aab826e
anirban222777das <anirban222777das@gmail.com>
2025-08-10 13:03:23 +0530

    day 1 complete

---
+++ basics/comparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1)
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null > 0);
+console.log(null == 0)
+console.log(null >= 0) 
+// avoid these kind of codes to avoid cofusion 
+
+// ===  [strict check]
+console.log("2" === 2);// false
+console.log("2" === 2);// true
