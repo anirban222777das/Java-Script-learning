d499580b755ac7553e992714d08e0c2ea18ad83a
Anirban Das <147873362+anirban222777das@users.noreply.github.com>
2025-08-10 12:02:23 +0530

    Create data_types.js

---
+++ basics/data_types.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // this only works in browser
+
+let name = "Anirban"
+let age = 20
+let isLoggedIn=false
+
+// number =>2 to power 53
+// bigint (for bigger interger)
+// string=> ""
+// boolean = true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 💀
+
+console.log(typeof "Anirban"); // typeof keyword to find the type of a variable
