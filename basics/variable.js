e5a7a82cc08b79c8c53e6329f8aa1da5877fe960
Anirban Das <147873362+anirban222777das@users.noreply.github.com>
2025-08-10 12:03:30 +0530

    Create variable.js

---
+++ basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 11212
+let accountEmail ="anirban@gmail.com"
+var accountPassword = "123456"
+accountCity="Kolkata"
+let accountState;
+
+//accountId=2
+accountPassword="1212344"
+accountCity="Delhi"
+accountEmail ="cadf@gmail.com"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
